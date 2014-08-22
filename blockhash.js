4e742d52a07cef66f1c3ac6a49ed789c27cdfa2a
Artem Popov <artfwo@gmail.com>
2014-08-22 18:18:07 +0700

    Overlapping blocks support in pixdiv.

--- blockhash.js
+++ blockhash.js
@@ -79,73 +79,110 @@
         return result;
     };
 
-    var method1_pixdiv = function(data, bits) {
-        var i, j, x, y, ii, avgvalue;
-        var weight_top, weight_bottom, weight_left, weight_right;
-        var block_top, block_bottom, block_left, block_right;
-        var y_mod, y_frac, y_int;
-        var x_mod, x_frac, x_int;
-
-        var block_width = data.width / bits;
-        var block_height = data.height / bits;
-
+    var method_pixdiv = function(data, bits, overlap) {
         var result = [];
 
-        // initialize blocks array with 0s
-        var blocks = [];
-        for (i = 0; i < bits; i++) {
-            blocks.push([]);
-            for (j = 0; j < bits; j++) {
-                blocks[i].push(0);
-            }
-        }
+        function onepass(block_width, block_height, x_offset, y_offset, maxblocks) {
+            var i, j, x, y, ii, avgvalue;
+            var weight_top, weight_bottom, weight_left, weight_right;
+            var block_top, block_bottom, block_left, block_right;
+            var y_mod, y_frac, y_int;
+            var x_mod, x_frac, x_int;
 
-        for (y = 0; y < data.height; y++) {
-            y_mod = (y + 1) % block_height;
-            y_frac = y_mod - Math.floor(y_mod);
-            y_int = y_mod - y_frac;
-
-            weight_top = (1 - y_frac);
-            weight_bottom = (y_frac);
-
-            // y_int will be 0 on bottom/right borders and on block boundaries
-            if (y_int > 0 || (y + 1) === data.height) {
-                block_top = block_bottom = Math.floor(y / block_height);
-            } else {
-                block_top = Math.floor(y / block_height);
-                block_bottom = Math.ceil(y / block_height);
+            // initialize blocks array with 0s
+            var blocks = [];
+            for (i = 0; i < maxblocks; i++) {
+                blocks.push([]);
+                for (j = 0; j < maxblocks; j++) {
+                    blocks[i].push(0);
+                }
             }
 
-            for (x = 0; x < data.width; x++) {
-                var ii = (y * data.width + x) * 4;
-                var avgvalue = (data.data[ii] + data.data[ii+1] + data.data[ii+2] + data.data[ii+3]) / 4.0;
+            for (y = y_offset; y < data.height; y++) {
+                y_mod = (y - y_offset + 1) % block_height;
+                y_frac = y_mod - Math.floor(y_mod);
+                y_int = y_mod - y_frac;
 
-                x_mod = (x + 1) % block_width;
-                x_frac = x_mod - Math.floor(x_mod);
-                x_int = x_mod - x_frac;
+                weight_top = (1 - y_frac);
+                weight_bottom = (y_frac);
 
-                weight_left = (1 - x_frac);
-                weight_right = x_frac;
-
-                // x_int will be 0 on bottom/right borders and on block boundaries
-                if (x_int > 0 || (x + 1) === data.width) {
-                    block_left = block_right = Math.floor(x / block_width);
+                // y_int will be 0 on bottom/right borders and on block boundaries
+                if (y_int > 0 || (y + 1) === data.height) {
+                    block_top = block_bottom = Math.floor((y - y_offset) / block_height);
                 } else {
-                    block_left = Math.floor(x / block_width);
-                    block_right = Math.ceil(x / block_width);
+                    block_top = Math.floor((y - y_offset) / block_height);
+                    block_bottom = Math.ceil((y - y_offset) / block_height);
                 }
 
-                // add weighted pixel value to relevant blocks
-                blocks[block_top][block_left] += avgvalue * weight_top * weight_left;
-                blocks[block_top][block_right] += avgvalue * weight_top * weight_right;
-                blocks[block_bottom][block_left] += avgvalue * weight_bottom * weight_left;
-                blocks[block_bottom][block_right] += avgvalue * weight_bottom * weight_right;
+                // stop after reaching maxblocks, in case we're doing 4-pass analysis with overlapping blocks
+                if (block_bottom == maxblocks) {
+                    break;
+                }
+
+                for (x = x_offset; x < data.width; x++) {
+                    var ii = (y * data.width + x) * 4;
+                    var avgvalue = (data.data[ii] + data.data[ii+1] + data.data[ii+2] + data.data[ii+3]) / 4.0;
+
+                    x_mod = (x - x_offset + 1) % block_width;
+                    x_frac = x_mod - Math.floor(x_mod);
+                    x_int = x_mod - x_frac;
+
+                    weight_left = (1 - x_frac);
+                    weight_right = x_frac;
+
+                    // x_int will be 0 on bottom/right borders and on block boundaries
+                    if (x_int > 0 || (x + 1) === data.width) {
+                        block_left = block_right = Math.floor((x - x_offset) / block_width);
+                    } else {
+                        block_left = Math.floor((x - x_offset) / block_width);
+                        block_right = Math.ceil((x - x_offset) / block_width);
+                    }
+
+                    // stop after reaching maxblocks, in case we're doing 4-pass analysis with overlapping blocks
+                    if (block_right == maxblocks) {
+                        break;
+                    }
+
+                    // add weighted pixel value to relevant blocks
+                    blocks[block_top][block_left] += avgvalue * weight_top * weight_left;
+                    blocks[block_top][block_right] += avgvalue * weight_top * weight_right;
+                    blocks[block_bottom][block_left] += avgvalue * weight_bottom * weight_left;
+                    blocks[block_bottom][block_right] += avgvalue * weight_bottom * weight_right;
+                }
             }
+            return blocks;
         }
 
-        for (i = 0; i < bits; i++) {
-            for (j = 0; j < bits; j++) {
-                result.push(blocks[i][j]);
+        if (overlap) {
+            var overlap_width = data.width / (bits + 1);
+            var overlap_height = data.height / (bits + 1);
+            var block_width = overlap_width * 2;
+            var block_height = overlap_height * 2;
+
+            var blocks1 = onepass(block_width, block_height, 0, 0, Math.floor(bits / 2));
+            var blocks2 = onepass(block_width, block_height, Math.floor(overlap_width), 0, Math.floor(bits / 2));
+            var blocks3 = onepass(block_width, block_height, 0, Math.floor(overlap_height), Math.floor(bits / 2));
+            var blocks4 = onepass(block_width, block_height, Math.floor(overlap_width), Math.floor(overlap_height), Math.floor(bits / 2));
+
+            for (i = 0; i < bits / 2; i++) {
+                for (j = 0; j < bits / 2; j++) {
+                    result.push(blocks1[i][j]);
+                    result.push(blocks2[i][j]);
+                }
+                for (j = 0; j < bits / 2; j++) {
+                    result.push(blocks3[i][j]);
+                    result.push(blocks4[i][j]);
+                }
+            }
+        } else {
+            var block_width = data.width / bits;
+            var block_height = data.height / bits;
+            var blocks = onepass(block_width, block_height, 0, 0, bits);
+
+            for (i = 0; i < bits; i++) {
+                for (j = 0; j < bits; j++) {
+                    result.push(blocks[i][j]);
+                }
             }
         }
 
@@ -208,7 +245,10 @@
                     hash = method2(imgData, bits);
                 }
                 else if (method === 3) {
-                    hash = method1_pixdiv(imgData, bits);
+                    hash = method_pixdiv(imgData, bits, false);
+                }
+                else if (method === 4) {
+                    hash = method_pixdiv(imgData, bits, true);
                 }
                 else {
                     throw new Error("Bad hashing method");
